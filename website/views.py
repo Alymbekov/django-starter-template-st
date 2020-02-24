aa9304d980eecfd8f96942b96a718bd5dbbba963
Alymbekov <maxim.makarov.1997@mail.ru>
2020-02-24 13:00:40 +0600

    initial commit

---
+++ website/views.py
@@ -0,0 +1,11 @@
+from django.shortcuts import render
+
+
+def index(request):
+    return render(request, 'website/index.html', {})
+
+def generic(request):
+    return render(request, 'website/generic.html', {})
+
+def elements(request):
+    return render(request, 'website/elements.html', {})
